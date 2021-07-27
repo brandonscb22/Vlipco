c8318857909d759b1a7201668d77b6593cbb189e
BRANDON LOEPZ <brandonscb95@gmail.com>
2021-07-27 00:39:01 -0500

    change of description api travels

--- app/controllers/api/v1/travels.rb
+++ app/controllers/api/v1/travels.rb
@@ -3,13 +3,13 @@ module API
     class Travels < Grape::API
       include API::V1::Defaults
       resource :travels do
-        desc "Return all users"
+        desc "Return all travels"
         get "" do
           Travel.all
         end
-        desc "Return a user"
+        desc "Return a travel"
         params do
-          requires :id, type: String, desc: "ID of the user"
+          requires :id, type: String, desc: "ID of the travel"
         end
         get ":id" do
           Travel.where(id: permitted_params[:id]).first!

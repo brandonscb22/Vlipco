5114a57e5d1ba874dd134b898a18974132832369
BRANDON LOEPZ <brandonscb95@gmail.com>
2021-07-30 03:14:51 -0500

    test

--- app/services/wompi/wompi_register_card.rb
+++ app/services/wompi/wompi_register_card.rb
@@ -4,6 +4,8 @@ class Wompi::WompiRegisterCard < ApplicationService
   attr_reader :message
 
   def initialize(body)
+    puts Rails.env
+    puts Rails.application.credentials.WOMPI_URL
     @url = Rails.application.credentials.WOMPI_URL + '/tokens/cards'
     @body = body
   end

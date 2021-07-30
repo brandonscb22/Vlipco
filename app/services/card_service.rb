4fada216c749d170076a72121c9309046d01f419
BRANDON LOEPZ <brandonscb95@gmail.com>
2021-07-30 11:09:20 -0500

    test

--- app/services/card_service.rb
+++ app/services/card_service.rb
@@ -25,49 +25,53 @@ class CardService < ApplicationService
   end
 
   def create(params)
-    # search user for email
-    user = User.find_by(email: params['email'])
-    if user
-      # send info credit card
-      wrt = Wompi::WompiRegisterCard.new(params['card'])
-      body = wrt.call
-      if body[:status] === 'CREATED'
-        # token credit card obtain wompi
-        tokenCard = body[:data][:id]
-        wot = Wompi::WompiObtainToken.new
-        body = wot.call
-        # acceptance_token
-        token = body[:data][:presigned_acceptance][:acceptance_token]
-        requestSaveCard = {
-          "type": "CARD",
-          "token": tokenCard,
-          "customer_email": params['email'],
-          "acceptance_token": token
-        }
-        wsc = Wompi::WompiSaveCard.new(requestSaveCard)
-        body = wsc.call
-        idCard = body[:data][:id]
-        # create association credit card -> user
-        card = Card.create(token: tokenCard,typeCard: 'CARD',idCard:idCard,user: user)
-        return {
-          success: true,
-          data: card,
-          message: 'OK'
-        }
+    begin
+      # search user for email
+      user = User.find_by(email: params['email'])
+      if user
+        # send info credit card
+        wrt = Wompi::WompiRegisterCard.new(params['card'])
+        body = wrt.call
+        if body[:status] === 'CREATED'
+          # token credit card obtain wompi
+          tokenCard = body[:data][:id]
+          wot = Wompi::WompiObtainToken.new
+          body = wot.call
+          # acceptance_token
+          token = body[:data][:presigned_acceptance][:acceptance_token]
+          requestSaveCard = {
+            "type": "CARD",
+            "token": tokenCard,
+            "customer_email": params['email'],
+            "acceptance_token": token
+          }
+          wsc = Wompi::WompiSaveCard.new(requestSaveCard)
+          body = wsc.call
+          idCard = body[:data][:id]
+          # create association credit card -> user
+          card = Card.create(token: tokenCard,typeCard: 'CARD',idCard:idCard,user: user)
+          return {
+            success: true,
+            data: card,
+            message: 'OK'
+          }
+        else
+          return {
+            success: false,
+            error: 'card load failed',
+            message:'card load failed'
+          }
+        end
       else
+        status 404
         return {
           success: false,
-          error: 'card load failed',
-          message:'card load failed'
+          error: 'user ('+ params['email'] +') not found',
+          message:'user not found'
         }
       end
-    else
-      status 404
-      return {
-        success: false,
-        error: 'user ('+ params['email'] +') not found',
-        message:'user not found'
-      }
+    rescue => error
+      p error.message
     end
   end
 
